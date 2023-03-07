6ad386c48fe4048c2061671bc7a1017d19b5de73
baaqi <baaqi@DESKTOP-HURPVQQ>
2023-03-07 11:24:23 +0530

    Add project files.

---
+++ Learns.Model/Classes/Institute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learns.Model.Classes
+{
+    public class Institute
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Learns.Model/Classes/LearnsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learns.Model.Classes
+{
+    public class LearnsContext : DbContext
+    {
+        public LearnsContext(DbContextOptions<LearnsContext> options) : base(options) { }
+
+        public DbSet<Institute> Institutes { get; set; }
+    }
+}
