bd925868207ced7b3962388e06d9dfafca921ff6
sajidhassan1 <sajidhasan15@gmail.com>
2023-03-14 08:18:27 +0530

    add field to topic

--- Learns.Model/Classes/Topic.cs
+++ Learns.Model/Classes/Topic.cs
@@ -8,6 +8,7 @@ namespace Learns.Model.Classes
     {
         public long ID { get; set; }
         public string TopicName { get; set; }
+        public bool IsActive { get; set; }
         public string SubjectId { get; set; }
     }
 }
